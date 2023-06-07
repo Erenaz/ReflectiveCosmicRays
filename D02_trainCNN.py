2895d98073edcdef6362c292eb2005e93ed07c8e
Erenaz <81892835+Erenaz@users.noreply.github.com>
2023-06-07 13:45:21 -0700

    Update D02_trainCNN.py

--- D02_trainCNN.py
+++ D02_trainCNN.py
@@ -20,7 +20,7 @@ path = f'Code/data/{round}/'
 station = 14  # Change this value to match the station you are working with
 
 # Change this value to control how many times the simulation file is used
-simulation_multiplier = 1  # Use the simulation file twice for training
+simulation_multiplier = 1  
 
 # Get a list of the RCR files
 RCR_files = glob(os.path.join(path, "ReflCR_5730events_part0.npy"))
@@ -35,13 +35,50 @@ TrainCut = len(RCR)
 # Get a list of all the Noise files
 Noise_files = glob(os.path.join(path, f"Station{station}_Data_*_part*.npy"))
 Noise = np.empty((0, 4, 256))
-for file in Noise_files:
-    Noise = np.concatenate((Noise, np.load(file)[0:5000, 0:4]))
 
-index = np.arange(0, len(Noise), 1)
-np.random.shuffle(index)
-Noise = Noise[index]
-Noise = Noise[0:TrainCut, 0:4]
+# Pick one every 200 events from Noise data
+pick_rate = 200
+chunk_size = 50000
+
+selected_events_count = 0  # Keep track of total selected events
+
+for file in Noise_files:
+    # Load the noise data from the current file
+    Noise_file = np.load(file)
+    num_chunks = Noise_file.shape[0] // chunk_size
+    if Noise_file.shape[0] % chunk_size:
+        num_chunks += 1
+
+    for chunk in range(num_chunks):
+        start = chunk * chunk_size
+        end = min(start + chunk_size, Noise_file.shape[0])
+
+        # Extract the chunk from the data
+        Noise_chunk = Noise_file[start:end]
+
+        # Select one every 200 events
+        selected_events = Noise_chunk[np.arange(0, Noise_chunk.shape[0], pick_rate)]
+
+        # Concatenate the selected events
+        Noise = np.concatenate((Noise, selected_events))
+        selected_events_count += len(selected_events)
+
+        # Clear Noise_chunk from memory
+        del Noise_chunk
+        del selected_events
+
+        # Break if total selected events count reached 5000
+        if selected_events_count >= 5000:
+            break
+
+    # Break outer loop if total selected events count reached 5000
+    if selected_events_count >= 5000:
+        break
+
+    # Clear Noise_file from memory
+    del Noise_file
+
+Noise = Noise[:TrainCut]
 
 #make signal the same shape as the noise data, if needed
 #Reuse one set multiple times to match larger dataset of the other
@@ -66,16 +103,15 @@ x = x[s]
 y = y[s]
 print(x.shape)
 
-BATCH_SIZE = 32
-#Iterate over many epochs to see which has lowest loss
-#Then change epochs to be at lowest for final result
-EPOCHS = 30
+BATCH_SIZE = 32 #Iterate over many epochs to see which has lowest loss
+EPOCHS = 100 #Then change epochs to be at lowest for final result
 
 #This automatically saves when loss increases over a number of patience cycles
 callbacks_list = [keras.callbacks.EarlyStopping(monitor='val_loss', patience=2)]
 def training(j):
     model = Sequential()
-    model.add(Conv2D(10, (4, 10), activation='relu', input_shape=(n_channels, n_samples, 1)))
+    model.add(Conv2D(20, (4, 10), activation='relu', input_shape=(n_channels, n_samples, 1))) # Could be 10, (4, 256/?)
+    model.add(Conv2D(10, (1, 10), activation='relu'))
     model.add(Dropout(0.5))
     model.add(Flatten())
     model.add(Dense(1, activation='sigmoid'))
@@ -86,7 +122,7 @@ def training(j):
     history = model.fit(x, y, validation_split=0.2, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1, callbacks=callbacks_list)
 
     # Save the history as a pickle file
-    with open(f'Code/h5_models/14/history_{j}_{simulation_multiplier}.pickle', 'wb') as f:
+    with open(f'Code/h5_models/14/history_CNN_2l-20-4-10-10-1-10_do0.5_fltn_sigm_valloss_p4_measNoise0-20k_0-5ksigNU-Scaled_shuff_monitortraining_{j}_M{simulation_multiplier}.pickle', 'wb') as f:
         pickle.dump(history.history, f)
 
     # Plot the training and validation loss
@@ -99,7 +135,7 @@ def training(j):
     plt.title(f'Model {j+1}: Simulation File Used {simulation_multiplier} Times')
 
     # Save the loss plot as an image file
-    plt.savefig(f'Code/h5_models/14/loss_plot_{j}_{simulation_multiplier}.png')
+    plt.savefig(f'Code/h5_models/14/loss_plot_CNN_2l-20-4-10-10-1-10_do0.5_fltn_sigm_valloss_p4_measNoise0-20k_0-5ksigNU-Scaled_shuff_monitortraining_{j}_{simulation_multiplier}.png')
 
     # Plot the training and validation accuracy
     plt.figure(figsize=(6, 4))
@@ -111,9 +147,9 @@ def training(j):
     plt.title(f'Model {j+1}: Simulation File Used {simulation_multiplier} Times')
 
     # Save the accuracy plot as an image file
-    plt.savefig(f'Code/h5_models/14/accuracy_plot_{j}_{simulation_multiplier}.png')
+    plt.savefig(f'Code/h5_models/14/accuracy_plot_CNN_2l-20-4-10-10-1-10_do0.5_fltn_sigm_valloss_p4_measNoise0-20k_0-5ksigNU-Scaled_shuff_monitortraining{j}_{simulation_multiplier}.png')
 
-    plt.show()
+    #plt.show()
 
     model.summary()
 
@@ -123,9 +159,8 @@ def training(j):
     print(f'Validation Accuracy: {val_acc}')
 
     #input the path and file you'd like to save the model as (in h5 format)
-    model.save(f'Code/h5_models/14/{round}_trained_CNN_1l-10-8-10_do0.5_fltn_sigm_valloss_p4_measNoise0-20k_0-5ksigNU-Scaled_shuff_monitortraining_{j}_{simulation_multiplier}.h5')
+    model.save(f'Code/h5_models/14/{round}_trained_CNN_2l-20-4-10-10-1-10_do0.5_fltn_sigm_valloss_p4_measNoise0-20k_0-5ksigNU-Scaled_shuff_monitortraining_{j}_{simulation_multiplier}.h5')
   
 #can increase the loop for more trainings is you want to see variation
 for j in range(1):
   training(j)
-
